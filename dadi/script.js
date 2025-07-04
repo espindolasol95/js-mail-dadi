a9c0cc83370249d0a2a848f74f397924862c3899
espindolasol95 <espindolasol.ok@gmail.com>
2025-07-04 18:39:07 +0200

    feat(random number for computer (1-6) )

---
+++ dadi/script.js
@@ -0,0 +1,14 @@
+//### Dadi:
+//- Generare due numeri casuali (giocatore e computer)
+//- Confrontare i due numeri
+//- Stampare chi ha vinto, pareggio o perso
+
+//random number for the player (1-6)
+const player = Math.floor (Math.random()*6) + 1;
+console.log ("player" , player);
+
+
+//random number for computer (1-6)
+
+const computer= Math.floor (Math.random()*6) + 1;
+console.log ("computer" , computer);
--- newFile.js
+++
@@ -1,8 +0,0 @@
-//### Dadi:
-//- Generare due numeri casuali (giocatore e computer)
-//- Confrontare i due numeri
-//- Stampare chi ha vinto, pareggio o perso
-
-
-const player = Math.floor (Math.random()*6) + 1;
-console.log ("player" , player);
