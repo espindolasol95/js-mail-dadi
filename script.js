48ad7766fd0c7fbf8fcfce2f50268e4a368deeee
espindolasol95 <espindolasol.ok@gmail.com>
2025-07-04 16:27:41 +0200

    feat (email request to user)

--- script.js
+++ script.js
@@ -2,4 +2,5 @@
 // list of party
 const listEmail = ["goku@hotmail.com , vegeta@hotmail.com , crilin@hotmail.com , gohan@hotmail.com" ]
 
-//
+//email request to user
+const UserEmail = prompt ("enter your email");
